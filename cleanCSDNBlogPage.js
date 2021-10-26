a4aade93ab8f913c2095e06795896fc3bfe7d9c1
Howone <unknownhowone@gmail.com>
2021-10-27 03:51:55 +0800

    增加了清洁CSDN博客详情页的js脚本

---
+++ cleanCSDNBlogPage.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         CSDN-Blog详情页
+// @namespace    https://blog.csdn.net/*
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://blog.csdn.net/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(() => {
+    const content = document.querySelector('.blog-content-box')
+    content.style.display = 'relative'
+    content.style.left = -150 + 'px'
+    document.querySelector('#mainBox > aside').parentElement.removeChild(document.querySelector('#mainBox > aside'))
+    document.querySelector('#csdn-toolbar').parentElement.removeChild(document.querySelector('#csdn-toolbar'))
+    document.querySelector('.csdn-side-toolbar').parentElement.removeChild(document.querySelector('.csdn-side-toolbar'))
+    document.querySelector('#toolBarBox').parentElement.removeChild(document.querySelector('#toolBarBox'))
+    document.querySelector('#mainBox > main > div.first-recommend-box.recommend-box').parentElement.removeChild(document.querySelector('#mainBox > main > div.first-recommend-box.recommend-box'))
+    document.querySelector('#mainBox > main > div.second-recommend-box.recommend-box').parentElement.removeChild(document.querySelector('#mainBox > main > div.second-recommend-box.recommend-box'))
+    document.querySelector('#mainBox > main > div.comment-box > div.comment-edit-box.d-flex').parentElement.removeChild(document.querySelector('#mainBox > main > div.comment-box > div.comment-edit-box.d-flex'))
+    document.querySelector('#mainBox > main > div.recommend-box.insert-baidu-box').parentElement.removeChild(document.querySelector('#mainBox > main > div.recommend-box.insert-baidu-box'))
+    document.querySelector('#commentBox').parentElement.removeChild(document.querySelector('#commentBox'))
+    document.querySelector('#mainBox > main > div.comment-box').parentElement.removeChild(document.querySelector('#mainBox > main > div.comment-box'))
+    document.querySelector('#mainBox > main > div.template-box').parentElement.removeChild(document.querySelector('#mainBox > main > div.template-box'))
+    document.querySelector('#mainBox > main > div.blog-footer-bottom').parentElement.removeChild(document.querySelector('#mainBox > main > div.blog-footer-bottom'))
+    document.querySelector('#copyright-box').parentElement.removeChild(document.querySelector('#copyright-box'))
+})();
+
